895ee84b94557c08dd5152488f0d87bbe98ea4a8
Nurbu <lamanurbu2@gmail.com>
2026-04-15 14:21:04 -0400

    Overloading of CellPhone constructor and dialing method work.

--- src/main/java/pluralsight/CellPhone.java
+++ src/main/java/pluralsight/CellPhone.java
@@ -15,6 +15,15 @@ public class CellPhone {
         owner = "";
     }
 
+    // New constructor with parameters
+    public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner) {
+        this.serialNumber = serialNumber;
+        this.model = model;
+        this.carrier = carrier;
+        this.phoneNumber = phoneNumber;
+        this.owner = owner;
+    }
+
     // Getters and Setters
     public void setSerialNumber(int newSerialNumber) {
         serialNumber = newSerialNumber;
--- src/main/java/pluralsight/CellPhoneApplication.java
+++ src/main/java/pluralsight/CellPhoneApplication.java
@@ -10,21 +10,36 @@ public class CellPhoneApplication {
         // Create a CellPhone instance called newPhone
 
         CellPhone newPhone1 = new CellPhone();
-        CellPhone newPhone2 = new CellPhone();
+        // CellPhone newPhone2 = new CellPhone();
+        CellPhone newPhone3 = new CellPhone(1234567, "IPhone 17 Pro", "Verizon", "911", "Nurbu");
 
 
         // Printing phone information
         display(scanner, newPhone1);
-        display(scanner, newPhone2);
+        // display(scanner, newPhone2);
+        display(newPhone3);
 
         //Calling phones
-        newPhone1.dial(newPhone2.getPhoneNumber());
-        newPhone2.dial(newPhone1.getPhoneNumber());
+        newPhone1.dial(newPhone3.getPhoneNumber());
+        //newPhone2.dial(newPhone1.getPhoneNumber());
+        newPhone3.dial(newPhone1.getPhoneNumber());
 
         scanner.close();
     }
 
+    // Regular Display method
+    public static void display(CellPhone phone) {
+        // Used Getters to output information about newPhone
+        System.out.println(phone.getSerialNumber());
+        System.out.println(phone.getModel());
+        System.out.println(phone.getCarrier());
+        System.out.println(phone.getPhoneNumber());
+        System.out.println(phone.getOwner());
+    }
+
+    // Had Extra time created for fun.
     public static void display(Scanner scanner, CellPhone phone) {
+
         int serialNumber;
         String model;
         String carrier;
